6c282d71ec7184b70230096e95fd58f8f5695828
liuxiaoqing1001 <liuxiaoqing7073@126.com>
2020-10-07 09:40:06 +0800

    20201007更新1

--- rt_springboot_mybatis/news_rest_ful_user/src/main/java/com/example/liu/news/NewsApplication.java
+++ rt_springboot_mybatis/news_rest_ful_user/src/main/java/com/example/liu/news/NewsApplication.java
@@ -2,12 +2,21 @@ package com.example.liu.news;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
-public class NewsApplication {
+public class NewsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(NewsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(NewsApplication.class);
     }
 
 }
