120b9216c83e8660e1b0224ad26cff5d37cd8804
liuxiaoqing1001 <liuxiaoqing7073@126.com>
2020-12-11 19:42:20 +0800

    20201211更新4

---
+++ other/vvideo/src/main/java/com/example/liu/vvideo/VvideoApplication.java
@@ -0,0 +1,13 @@
+package com.example.liu.vvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VvideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VvideoApplication.class, args);
+    }
+
+}
---
+++ other/vvideo/src/test/java/com/example/liu/vvideo/VvideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liu.vvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VvideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
